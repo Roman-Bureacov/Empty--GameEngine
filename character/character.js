8219f98ed087c0d234f9f9db53ac80400981f4b8
RomaBureacov <RomanBureacov@gmail.com>
2026-01-18 19:26:21 -0800

    changed character to respect states more similar to that of enums than magic strings everywhere

--- character/character.js
+++ character/character.js
@@ -10,61 +10,87 @@ class character {
 
         this.position = {x: 0, y: 0};
         this.scale = {x: 2, y: 2};
-        this.state = "idle";
-        this.facing = "right";
         this.dx = 0;
 
+        this.states = Object.freeze({
+            MOVE : "move ",
+            ATTACK : "attack ",
+            IDLE : "idle ",
+        });
+
+        this.direction = Object.freeze({
+            LEFT : "left ",
+            RIGHT : "right ",
+        });
+
+        this.state = this.states.IDLE;
+        this.facing = this.direction.RIGHT;
+
+
         this.setupAnimation();
+        this.setupKeymap();
     }
 
     setupAnimation() {
         this.animations = {
-            "move right" : new Animator(
+            [this.states.MOVE + this.direction.RIGHT] : new Animator(
                 this.spritesheet,
                 [ [1, 0], [1, 1], [1, 2], [1, 3], [1, 4], [1, 5] ],
                 0.25),
-            "move left": new Animator(
+            [this.states.MOVE + this.direction.LEFT]: new Animator(
                 this.spritesheet,
                 [ [1, 13], [1, 12], [1, 11], [1, 10], [1, 9], [1, 8] ],
                 0.25
             ),
-            "idle right": new Animator(
+            [this.states.IDLE + this.direction.RIGHT]: new Animator(
                 this.spritesheet,
                 [ [0, 0] ],
                 1
                 ),
-            "idle left": new Animator(
+            [this.states.IDLE + this.direction.LEFT]: new Animator(
                 this.spritesheet,
                 [ [0, 13] ],
                 1
             ),
-            "attack right": new Animator(
+            [this.states.ATTACK + this.direction.RIGHT]: new Animator(
                 this.spritesheet,
                 [ [2, 0], [2, 1], [2, 2], [2, 3], [2, 4], [2, 5], [2, 6] ],
                 0.25,
                 false,
-                () => this.state = "idle"
+                () => {
+                    this.state = this.states.IDLE;
+                    this.facing = this.direction.RIGHT;
+                }
             ),
-            "attack left": new Animator(
+            [this.states.ATTACK + this.direction.LEFT]: new Animator(
                 this.spritesheet,
                 [ [2, 13], [2, 12], [2, 11], [2, 10], [2, 9], [2, 8], [2, 7] ],
                 0.25,
                 false,
-                () => this.state = "idle"
+                () => {
+                    this.state = this.states.IDLE;
+                    this.facing = this.direction.LEFT;
+                }
             ),
         };
 
         this.currentAnimation = this.animations[this.animationName()];
     }
 
+    setupKeymap() {
+        this.keymap = {
+
+        }
+    }
+
     move(dx) {
-        this.state = "move";
+        this.state = this.states.MOVE;
         this.dx = dx;
-        this.facing = dx < 0 ? "left" : "right";
+        this.facing = dx < 0 ? this.direction.LEFT : this.direction.RIGHT;
     }
 
     swing() {
-        this.state = "attack";
+        this.state = this.states.ATTACK;
     }
 
     draw(context) {
@@ -81,19 +107,19 @@ class character {
     }
 
     animationName() {
-        return this.state + " " + this.facing;
+        return this.state + this.facing;
     }
 
     update() {
         ({
-            "attack" : () => {
+            [this.states.ATTACK] : () => {
 
             },
-            "move" : () => {
+            [this.states.MOVE] : () => {
                 if (this.dx !== 0) {
                     this.position.x += this.dx;
                     this.dx = 0;
-                } else this.state = "idle"
+                } else this.state = this.states.IDLE;
             }
         })[this.state]?.();
     }
